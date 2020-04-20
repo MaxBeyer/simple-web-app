ff107929bc2e57b774d2ba21c1c47d1ba49d379d
Max Beyer <ZeusAndHisBeard@users.noreply.github.com>
2020-04-20 15:25:09 -0500

    remove quantity db value, use list size instead

--- src/main/java/com/zeus/examples/simplewebapp/db/FoodItem.java
+++ src/main/java/com/zeus/examples/simplewebapp/db/FoodItem.java
@@ -6,23 +6,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Entity
 @Getter @Setter @NoArgsConstructor @EqualsAndHashCode
 public class FoodItem {
     @Id
-    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
     private Integer id;
     @Column(name = "fidge_id")
     private Integer fridgeId;
     @Enumerated(EnumType.STRING)
     private FoodType foodType;
-    private Integer quantity;
 
     public FoodItem(Integer fridgeId, FoodType foodType, Integer quantity){
         this.fridgeId = fridgeId;
         this.foodType = foodType;
-        this.quantity = quantity;
     }
 }
