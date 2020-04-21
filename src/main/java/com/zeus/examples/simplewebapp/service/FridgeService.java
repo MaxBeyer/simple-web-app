00d5e0d72ad9aca623fcd779f68816020b7c932c
Max Beyer <ZeusAndHisBeard@users.noreply.github.com>
2020-04-21 11:02:09 -0500

    added controller tests, controller and service dummy objects

---
+++ src/main/java/com/zeus/examples/simplewebapp/service/FridgeService.java
@@ -0,0 +1,22 @@
+package com.zeus.examples.simplewebapp.service;
+
+import com.zeus.examples.simplewebapp.db.FoodItem;
+import com.zeus.examples.simplewebapp.domain.FoodType;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class FridgeService {
+    public FoodItem getFood(FoodType foodType) {
+        return null;
+    }
+
+    public void storeFood(UUID fridgeId, FoodType foodType) {
+
+    }
+
+    public void removeFood(UUID any, FoodType any1) {
+
+    }
+}
